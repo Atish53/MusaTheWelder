02639f4c0a17516fa00d4d9a50233433269ceb96
Atish Krishen Hariparsad <21825470@dut4life.ac.za>
2021-11-19 23:32:47 +0200

    Checkout Controller - No SaleQuote

---
+++ MusaTheWelder/Controllers/CheckoutController.cs
@@ -0,0 +1,354 @@
+﻿using MusaTheWelder.Models;
+using Syncfusion.Pdf;
+using Syncfusion.Pdf.Graphics;
+using Syncfusion.Pdf.Grid;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MusaTheWelder.Controllers
+{
+    public class CheckoutController : Controller
+    {
+        ApplicationDbContext dB = new ApplicationDbContext();
+
+        // GET: Checkout
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [Authorize]
+        public async Task<ActionResult> Payment() //The Method That Handles All Transactions
+        {
+            //Variables Required For Tables
+            double deliveryFee = 0;
+            int numProducts = 0;
+
+            //Get Current Cart
+            var cart = ShoppingCart.GetCart(this.HttpContext);
+
+            //Create New Sale
+            Sale sale = new Sale();
+            sale.SaleDate = DateTime.Now.ToString();
+
+
+            numProducts = cart.GetCount();
+
+            if (numProducts == 1)
+            {
+                deliveryFee = 60;
+            }
+            else if (numProducts <= 3)
+            {
+                deliveryFee = 80;
+            }
+            else
+            {
+                deliveryFee = 100;
+            }
+
+            //Customer Details
+            sale.Email = User.Identity.Name;
+            sale.CustomerName = User.Identity.GetName();
+            sale.Address = User.Identity.GetAddress();
+            sale.City = User.Identity.GetCity();
+            sale.State = User.Identity.GetState();
+            sale.PostalCode = User.Identity.GetPostalCode();
+            sale.Country = User.Identity.GetCountry();
+            sale.PhoneNumber = User.Identity.GetPhoneNo();
+
+            //Sale Total
+            double SaleFinal = cart.GetTotal();
+            sale.SaleTotal = SaleFinal + deliveryFee;
+
+            //Check product stock
+            var x = cart.GetCartItems();
+            List<Product> chck = (from q in dB.Products
+                                  select q).ToList();
+
+            foreach (var items in x)
+            {
+                foreach (Product c in chck)
+                {
+                    if (items.ProductId == c.ProductId)
+                    {
+                        c.ProductStock += items.Count; //Temporary stock check pause... Make this -= to reduce stock
+                        if (c.ProductStock == 0)
+                        {
+                            c.isActive = false; //Sets item to inactive due to stock being zero. 
+                        }
+                    }
+                }
+            }
+
+            
+            dB.Sales.Add(sale);
+            await dB.SaveChangesAsync();
+
+            double FinalCost = cart.GetTotal() + cart.GetDeliveryFee();
+
+            //SaleDetail Saved Using Helper Method
+            string orderId = sale.SaleId.ToString();
+            sale = cart.CreateOrder(sale);
+
+            //New Email.
+            //Creates a new PDF document
+            PdfDocument document = new PdfDocument();
+            //Adds page settings
+            document.PageSettings.Orientation = PdfPageOrientation.Portrait;
+            document.PageSettings.Margins.All = 50;
+            //Adds a page to the document
+            PdfPage page = document.Pages.Add();
+            PdfGraphics graphics = page.Graphics;
+            //Loads the image from disk
+            PdfImage image = PdfImage.FromFile(Server.MapPath("~/Images/EmailLogo.png"));
+            RectangleF bounds = new RectangleF(10, 10, 200, 200);
+            //Draws the image to the PDF page
+            page.Graphics.DrawImage(image, bounds);
+            PdfBrush solidBrush = new PdfSolidBrush(new PdfColor(126, 151, 173));
+            bounds = new RectangleF(0, bounds.Bottom + 90, graphics.ClientSize.Width, 30);
+            //Draws a rectangle to place the heading in that region.
+            graphics.DrawRectangle(solidBrush, bounds);
+            //Creates a font for adding the heading in the page
+            PdfFont subHeadingFont = new PdfStandardFont(PdfFontFamily.TimesRoman, 14);
+            //Creates a text element to add the invoice number
+            PdfTextElement element = new PdfTextElement("Invoice " + sale.SaleId.ToString() + " for" + " " + sale.CustomerName, subHeadingFont);
+            element.Brush = PdfBrushes.White;
+
+            //Draws the heading on the page
+            PdfLayoutResult res = element.Draw(page, new PointF(10, bounds.Top + 8));
+            string currentDate = "Date Purchased " + sale.SaleDate.ToString();
+            //Measures the width of the text to place it in the correct location
+            SizeF textSize = subHeadingFont.MeasureString(currentDate);
+            PointF textPosition = new PointF(graphics.ClientSize.Width - textSize.Width - 10, res.Bounds.Y);
+            //Draws the date by using DrawString method
+            graphics.DrawString(currentDate, subHeadingFont, element.Brush, textPosition);
+            PdfFont timesRoman = new PdfStandardFont(PdfFontFamily.TimesRoman, 10);
+            //Creates text elements to add the address and draw it to the page.
+            element = new PdfTextElement("Bill To " + sale.Address.ToString() + ", " + sale.City + ", " + sale.State, timesRoman);
+            element.Brush = new PdfSolidBrush(new PdfColor(126, 155, 203));
+            res = element.Draw(page, new PointF(10, res.Bounds.Bottom + 25));
+            element = new PdfTextElement("Total Price R" + sale.SaleTotal.ToString(), timesRoman);
+            element.Brush = new PdfSolidBrush(new PdfColor(126, 155, 203));
+            res = element.Draw(page, new PointF(10, res.Bounds.Bottom + 25));
+            PdfPen linePen = new PdfPen(new PdfColor(126, 151, 173), 0.70f);
+            PointF startPoint = new PointF(0, res.Bounds.Bottom + 3);
+            PointF endPoint = new PointF(graphics.ClientSize.Width, res.Bounds.Bottom + 3);
+            //Draws a line at the bottom of the address
+            graphics.DrawLine(linePen, startPoint, endPoint);
+
+            //Creates the datasource for the table
+            DataTable invoiceDetails = new DataTable();
+
+            //Add columns to the DataTable
+            invoiceDetails.Columns.Add("Product Name");
+            invoiceDetails.Columns.Add("Quantity");
+            invoiceDetails.Columns.Add("Price");
+
+            //Add rows to the DataTable
+            foreach (var item in sale.SaleDetails.Where(m => m.SaleId == sale.SaleId))
+            {
+                invoiceDetails.Rows.Add(new object[] { item.Product.ProductName, item.Quantity, item.Product.ProductPrice });
+            }
+
+
+            //Creates text elements to add the address and draw it to the page.
+
+
+
+            //Creates a PDF grid
+            PdfGrid grid = new PdfGrid();
+            //Adds the data source
+            grid.DataSource = invoiceDetails;
+            //Creates the grid cell styles
+            PdfGridCellStyle cellStyle = new PdfGridCellStyle();
+            cellStyle.Borders.All = PdfPens.White;
+            PdfGridRow header = grid.Headers[0];
+            //Creates the header style
+            PdfGridCellStyle headerStyle = new PdfGridCellStyle();
+            headerStyle.Borders.All = new PdfPen(new PdfColor(126, 151, 173));
+            headerStyle.BackgroundBrush = new PdfSolidBrush(new PdfColor(126, 151, 173));
+            headerStyle.TextBrush = PdfBrushes.White;
+            headerStyle.Font = new PdfStandardFont(PdfFontFamily.TimesRoman, 14f, PdfFontStyle.Regular);
+
+            //Adds cell customizations
+            for (int i = 0; i < header.Cells.Count; i++)
+            {
+                if (i == 0 || i == 1)
+                    header.Cells[i].StringFormat = new PdfStringFormat(PdfTextAlignment.Left, PdfVerticalAlignment.Middle);
+                else
+                    header.Cells[i].StringFormat = new PdfStringFormat(PdfTextAlignment.Right, PdfVerticalAlignment.Middle);
+            }
+
+            //Applies the header style
+            header.ApplyStyle(headerStyle);
+            cellStyle.Borders.Bottom = new PdfPen(new PdfColor(217, 217, 217), 0.70f);
+            cellStyle.Font = new PdfStandardFont(PdfFontFamily.TimesRoman, 12f);
+            cellStyle.TextBrush = new PdfSolidBrush(new PdfColor(131, 130, 136));
+            //Creates the layout format for grid
+            PdfGridLayoutFormat layoutFormat = new PdfGridLayoutFormat();
+            // Creates layout format settings to allow the table pagination
+            layoutFormat.Layout = PdfLayoutType.Paginate;
+            //Draws the grid to the PDF page.
+            PdfGridLayoutResult gridResult = grid.Draw(page, new RectangleF(new PointF(0, res.Bounds.Bottom + 40), new SizeF(graphics.ClientSize.Width, graphics.ClientSize.Height - 100)), layoutFormat);
+
+            MemoryStream outputStream = new MemoryStream();
+            document.Save(outputStream);
+            outputStream.Position = 0;
+
+            var invoicePdf = new System.Net.Mail.Attachment(outputStream, System.Net.Mime.MediaTypeNames.Application.Pdf);
+            string docname = "Invoice.pdf";
+            invoicePdf.ContentDisposition.FileName = docname;
+
+            MailMessage mail = new MailMessage();
+            string emailTo = sale.Email;
+            MailAddress from = new MailAddress("africanmagicsystem@gmail.com");
+            mail.From = from;
+            mail.Subject = "Your invoice for order number #" + sale.SaleId;
+            mail.Body = "Dear " + sale.CustomerName + ", find your invoice in the attached PDF document.";
+            mail.To.Add(emailTo);
+
+            mail.Attachments.Add(invoicePdf);
+
+            mail.IsBodyHtml = true;
+            SmtpClient smtp = new SmtpClient();
+            smtp.Host = "smtp.gmail.com";
+            smtp.EnableSsl = true;
+            NetworkCredential networkCredential = new NetworkCredential("africanmagicsystem@gmail.com", "zbpabilmryequenp");
+            smtp.UseDefaultCredentials = true;
+            smtp.Credentials = networkCredential;
+            smtp.Port = 587;
+            smtp.Send(mail);
+            //Clean-up.
+            //Close the document.
+            document.Close(true);
+            //Dispose of email.
+            mail.Dispose();
+
+
+            //Payment...
+            try
+            {
+                // Retrieve required values for the PayFast Merchant
+                string name = "AppDev2 Store Sale Number: #" + sale.SaleTotal;
+                string description = "This is a once-off and non-refundable payment. ";
+
+                string site = "https://sandbox.payfast.co.za/eng/process";
+                string merchant_id = "";
+                string merchant_key = "";
+
+                string paymentMode = System.Configuration.ConfigurationManager.AppSettings["PaymentMode"];
+
+                if (paymentMode == "test")
+                {
+                    site = "https://sandbox.payfast.co.za/eng/process?";
+                    merchant_id = "10000100";
+                    merchant_key = "46f0cd694581a";
+                }
+
+                // Build the query string for payment site
+
+                StringBuilder str = new StringBuilder();
+                str.Append("merchant_id=" + HttpUtility.UrlEncode(merchant_id));
+                str.Append("&merchant_key=" + HttpUtility.UrlEncode(merchant_key));
+                str.Append("&return_url=" + HttpUtility.UrlEncode(System.Configuration.ConfigurationManager.AppSettings["PF_ReturnURL"]));
+                str.Append("&cancel_url=" + HttpUtility.UrlEncode(System.Configuration.ConfigurationManager.AppSettings["PF_CancelURL"]));
+                str.Append("&notify_url=" + HttpUtility.UrlEncode(System.Configuration.ConfigurationManager.AppSettings["PF_NotifyURL"]));
+
+                str.Append("&m_payment_id=" + HttpUtility.UrlEncode(sale.SaleId.ToString()));
+                str.Append("&amount=" + HttpUtility.UrlEncode(FinalCost.ToString()));
+                str.Append("&item_name=" + HttpUtility.UrlEncode(name));
+                str.Append("&item_description=" + HttpUtility.UrlEncode(description));
+
+                // Redirect to PayFast
+                return Redirect(site + str.ToString());
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        // GET: Checkout/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Checkout/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Checkout/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Checkout/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Checkout/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Checkout/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Checkout/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
